df4123ce054dee1c256ec80663fd01af6ef729c2
DinoJay <jott88@googlemail.com>
2023-04-01 19:15:20 +0200

    fixed rotate in upset

--- src/lib/components/CardView/Upset/index.svelte
+++ src/lib/components/CardView/Upset/index.svelte
@@ -10,64 +10,54 @@
 	const labelHeight = '8rem';
 </script>
 
-<div class="flex-grow flex  flex-col m-auto overflow-y-auto overflow-x-visible">
-	<div class="flex items-center overflow-visible mb-3">
-		{#each topics as t}
+<div class="flex items-center  m-auto mb-3">
+	{#each topics as t}
+		<div class="w-6 relative" style:height={labelHeight} style:margin-right={margin}>
 			<div
-				class="flex-none w-6 relative overflow-visible"
+				class="pt-2  crop "
+				style:background={t.color}
 				style:height={labelHeight}
-				style:margin-right={margin}
+				style:writing-mode="vertical-rl"
+				style:text-orientation="mixed"
+				style:transform-origin="left bottom"
+				style:transform="rotate(-20deg)"
+				style:isolation="isolate"
 			>
-				<div
-					class="pt-2 crop"
-					style:background={t.color}
-					style:height={labelHeight}
-					style:writing-mode="vertical-rl"
-					style:text-orientation="mixed"
-				>
-					{t.title}
-				</div>
+				{t.title}
+			</div>
+		</div>
+	{/each}
+</div>
+<div class="flex-grow flex  flex-col m-auto overflow-auto">
+	<div class="flex-grow flex flex-col overflow-y-auto">
+		{#each cards as c, j}
+			{@const lastIndex = topics?.findLastIndex((t) => c.topicIds.includes(t.id))}
+			{@const firstIndex = topics?.findIndex((t) => c.topicIds.includes(t.id))}
+			<div class="flex items-center">
+				<!-- <div class="w-32">{c.title}</div> -->
+				{#each topics as t, i}
+					{@const topic = c.topics?.find((t2) => t2.id === t.id)}
+					<div class="relative">
+						<div
+							on:click={() => (selectedRow = i)}
+							on:keydown={() => (selectedRow = i)}
+							class=" w-6 h-6 border-2 mb-3  {topic !== undefined ? 'border-gray-700' : 'bg-white'}"
+							style:background={topic !== undefined ? t.color : 'white'}
+						/>
+						{#if topic === undefined && i > firstIndex && i <= lastIndex}
+							<div
+								class="w-12 h-3  bg-gray-700 absolute left-0 top-0"
+								style:transform="translateY(50%)"
+							/>
+						{/if}
+					</div>
+					{#if i < lastIndex && i >= firstIndex}
+						<div class="h-3 bg-gray-700 " style:width={margin} style:transform="translateY(-50%)" />
+					{:else}
+						<div class="" style:width={margin} style:transform="translateY(-50%)" />
+					{/if}
+				{/each}
 			</div>
 		{/each}
-	</div>
-
-	<div class="flex overflow-y-auto">
-		<div class=" flex flex-col">
-			{#each cards as c, j}
-				{@const lastIndex = topics?.findLastIndex((t) => c.topicIds.includes(t.id))}
-				{@const firstIndex = topics?.findIndex((t) => c.topicIds.includes(t.id))}
-				<div class="flex items-center">
-					<!-- <div class="w-32">{c.title}</div> -->
-					{#each topics as t, i}
-						{@const topic = c.topics?.find((t2) => t2.id === t.id)}
-						<div class="relative">
-							<div
-								on:click={() => (selectedRow = i)}
-								on:keydown={() => (selectedRow = i)}
-								class=" w-6 h-6 border-2 mb-3  {topic !== undefined
-									? 'border-gray-700'
-									: 'bg-white'}"
-								style:background={topic !== undefined ? t.color : 'white'}
-							/>
-							{#if topic === undefined && i > firstIndex && i <= lastIndex}
-								<div
-									class="w-12 h-3  bg-gray-700 absolute left-0 top-0"
-									style:transform="translateY(50%)"
-								/>
-							{/if}
-						</div>
-						{#if i < lastIndex && i >= firstIndex}
-							<div
-								class="h-3 bg-gray-700 "
-								style:width={margin}
-								style:transform="translateY(-50%)"
-							/>
-						{:else}
-							<div class="" style:width={margin} style:transform="translateY(-50%)" />
-						{/if}
-					{/each}
-				</div>
-			{/each}
-		</div>
 	</div>
 </div>
